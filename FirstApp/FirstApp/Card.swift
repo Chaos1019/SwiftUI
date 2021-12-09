8d451d4bc21075e61cdd3ed28b0bfdb2cad19101
Shawn Locke <chaos1019@icloud.com>
2021-12-09 17:45:40 +0800

    增加Card模型

---
+++ FirstApp/FirstApp/Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  FirstApp
+//
+//  Created by 骆超然 on 2021/12/9.
+//
+
+import SwiftUI
+
+struct Card: Identifiable {
+    var id = UUID()
+    var title: String
+    var headline: String
+    var imageName: String
+    var callToAction: String
+    var message: String
+    var gradient: [Color]
+}
