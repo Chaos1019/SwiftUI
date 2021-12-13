5fd499fb4325bcfd3296c3ea10b09a65b2ff819f
Shawn Locke <chaos1019@icloud.com>
2021-12-13 22:19:24 +0800

    初始化第2章项目

---
+++ ThisIsBeijing/ThisIsBeijing/ThisIsBeijingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThisIsBeijingApp.swift
+//  ThisIsBeijing
+//
+//  Created by 骆超然 on 2021/12/13.
+//
+
+import SwiftUI
+
+@main
+struct ThisIsBeijingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
